df817674c26664f90261368fb433b1c2e73e494b
unknown <elarrin@gmail.com>
2020-12-10 17:55:56 +0300

    начинаем работать с валидацией

--- js/validate.js
+++ js/validate.js
@@ -7,7 +7,7 @@ const toggleSubmitButton = (formElement, inputList, params) => {
     )
     // console.log(hasInvalidInput)
     if (hasInvalidInput) {
-        buttonElement.classList.add(params.inactiveButtonClass)
+        buttonElement.classList.add(params.inactiveButtonClass)evt.preventDefault
         buttonElement.setAttribute("disabled", "true");
     } else {
         buttonElement.classList.remove(params.inactiveButtonClass)
