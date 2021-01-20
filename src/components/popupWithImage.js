1ef4ac39dd36ddb42c4271a19cb10cd700e2aa23
unknown <elarrin@gmail.com>
2021-01-20 11:39:48 +0300

    исправил последний комментарий от ревью

--- src/components/popupWithImage.js
+++ src/components/popupWithImage.js
@@ -2,14 +2,15 @@ import Popup from './Popup.js'
 
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
-        super(popupSelector)
-        this._fullSizeImg = this._popupElement.querySelector('.popup__full-size-img')
+        super(popupSelector);
+        this._fullSizeImg = this._popupElement.querySelector('.popup__full-size-img');
+        this._titleGalary = this._popupElement.querySelector('.popup__title_galary');
     }
 
     open(nameCard, linkCard) {
         super.open()
         this._fullSizeImg.src = linkCard
         this._fullSizeImg.alt = nameCard
-        this._popupElement.querySelector('.popup__title_galary').textContent = nameCard
+        this._titleGalary.textContent = nameCard
     }
 }
